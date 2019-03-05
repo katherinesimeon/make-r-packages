650b3c0b188b25067df9b2c3006c92e53bdbbd1d
Katherine <katherine@rladies.org>
2019-03-05 15:04:26 -0600

    updating slides

--- mathFun/R/minus_ten.R
+++ mathFun/R/minus_ten.R
@@ -1,3 +1,15 @@
+#' Minus Ten
+#'
+#' Subtract 10 from a given number
+#'
+#' @param number a number
+#'
+#' @return a number that is the provided number minus 10
+#' @export
+#'
+#' @examples
+#' minus_ten(22) will return 12
+#' minus_ten(22:25) will return 12 13 14 15
 minus_ten <- function(number) {
   number-10
 }
